95abc26e21777273281ca8862e098cbc8f4a51ce
laramartin <lmartin@babbel.com>
2018-10-16 21:04:35 +0200

    Add dark orange theme

--- flutter_news/lib/main.dart
+++ flutter_news/lib/main.dart
@@ -21,6 +21,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
       home: MyHomePage(
         title: 'Flutter Demo Home Page',
         bloc: bloc,
